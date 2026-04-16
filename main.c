eddf8bcca8a4da9849b79134c46cb5e8cd1e85f6
Felipe <pesilva.felipe@gmail.com>
2026-04-16 15:44:27 -0300

    Início da implementação do algortimo Quick Sort

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <time.h>
 
 // 1. Criar uma LinkedList
-// 2. Definir os tipos de dados (int e  string) 
+// 2. Definir os tipos de dados (int e  string)
 // 3. Criar as funções da Lista
 // 4. Adicionar as funções de ordenação (Selection, Merge e Quick Sort)
 // 5. Criar script gerador de dados
@@ -34,7 +34,7 @@ void insertInt(NodeInt** head, int value) {
         *head = newNode;
         return;
     }
-    
+
     // Se não, percorre até o final e insere
     NodeInt* temp = *head;
     while (temp->next != NULL) {
@@ -64,12 +64,12 @@ NodeStr* createNodeStr(char* text) {
 // 3. Função para inserir no final da lista de Strings
 void insertStr(NodeStr** head, char* text) {
     NodeStr* newNode = createNodeStr(text);
-    
+
     if (*head == NULL) {
         *head = newNode;
         return;
     }
-    
+
     NodeStr* temp = *head;
     while (temp->next != NULL) {
         temp = temp->next;
@@ -78,26 +78,70 @@ void insertStr(NodeStr** head, char* text) {
 }
 
 
+int* quickSortInteiros(NodeInt* head, int* tamanho_retorno) {
+    if (head == NULL) return NULL;
+
+    int tamanho = 0;
+    NodeInt* temp = head;
+    while (temp != NULL) {
+        tamanho++;
+        temp = temp->next;
+    }
+
+    int* vetor = (int*)malloc(tamanho * sizeof(int));
+    if (vetor == NULL) {
+        printf("Erro de alocação de memória para o vetor!\n");
+        exit(1);
+    }
+
+    temp = head;
+    int i = 0;
+    while (temp !=  NULL) {
+        vetor[i] = temp->data;
+        temp = temp->next;
+        i++;
+    }
+
+    *tamanho_retorno = tamanho;
+    return vetor;
+}
+
+
 int main(int argc, char * argv[]) {
     NodeInt* listInt = NULL;
     NodeStr* listStr = NULL;
 
+    // Lê o arquivo de inteiros e retorna se a leitura foi feita corretamente
     FILE* fptr = fopen("dados_teste/inteiros_100.txt", "r");
-
     if(fptr == NULL) {
-        printf("Erro: Nao foi possivel abrir o arquivo de inteiros!\n");
+        printf("Erro: Não foi possível abrir o arquivo de inteiros!\n");
         return 1; // Encerra o programa com erro
     }
 
-    int read_value;
+    int read_value_int;
 
-    while (fscanf(fptr, "%d", &read_value) == 1) {
-        insertInt(&listInt, read_value); // Insere o valor lido na nossa lista
+    while (fscanf(fptr, "%d", &read_value_int) == 1) {
+        insertInt(&listInt, read_value_int); // Insere o valor lido na lista
     }
-    
+
     fclose(fptr);
     printf("Arquivo lido e lista de inteiros preenchida com sucesso!\n");
 
+
+    // Lê o arquivo de strings e retorna se a leitura foi feita corretamente
+    fptr = fopen("dados_teste/strings_100.txt", "r");
+    if(fptr == NULL) {
+        printf("Erro: Não foi possível abrir o arquivo de strings!\n");
+        return 1;
+    }
+
+    char read_string[50];
+    while (fscanf(fptr, "%s", read_string) == 1) {
+        insertStr(&listStr, read_string); // Insere o valor lido na lista
+    }
+
+    fclose(fptr);
+    printf("Arquivo lido e lista de strings preenchida com sucesso!\n");
 
     return 0;
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import System.nanoTime;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
